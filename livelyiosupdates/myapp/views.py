9990270f6b16c368ffe398c86e1b8e56625dd87a
Ignacio Rodrigo <irodrigo@codigodelsur.com>
2013-02-01 15:25:54 -0200

    Fixed view

--- livelyiosupdates/myapp/views.py
+++ livelyiosupdates/myapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirectBase
+
+class LivelyHttpResponseRedirect(HttpResponseRedirectBase):
+	allowed_schemes = ['http', 'https', 'ftp', 'itms-services']
+	status_code = 302
+
 
 def index(request):
-    return redirect("itms-services://?action=download-manifest&url=itms-services://?action=download-manifest&url=https://lively-enterprise.s3.amazonaws.com/lively.plist")
-    
+	return LivelyHttpResponseRedirect("itms-services://?action=download-manifest&url=itms-services://?action=download-manifest&url=https://lively-enterprise.s3.amazonaws.com/lively.plist")
